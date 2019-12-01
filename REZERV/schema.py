d83de59cf1d46bbecc7337684ff1198c786e71c2
unknown <omarraiesecto@gmail.com>
2019-12-01 22:32:03 +0200

    rezerv

---
+++ REZERV/schema.py
@@ -0,0 +1,15 @@
+import graphene
+import REZERVAPP.schema
+
+
+class Query(REZERVAPP.schema.Query, graphene.ObjectType):
+    # This class will inherit from multiple Queries
+    # as we begin to add more apps to our project
+    pass
+
+class Mutation(REZERVAPP.schema.Mutation, graphene.ObjectType):
+    # This class will inherit from multiple Queries
+    # as we begin to add more apps to our project
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
