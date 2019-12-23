3c320b3a073a9122178cea713eb3b49d14bc6d6e
unknown <omarraiesecto@gmail.com>
2019-12-23 17:42:26 +0200

    last version

--- REZERV/settings.py
+++ REZERV/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'REZERVAPP',
     'graphene_django',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,8 +51,17 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
-
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = [
+'http://localhost:8000',
+]
+CORS_ORIGIN_REGEX_WHITELIST = [
+'http://localhost:8000',
+]
 ROOT_URLCONF = 'REZERV.urls'
 
 TEMPLATES = [
